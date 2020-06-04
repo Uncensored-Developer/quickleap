71ff5a773335d1dee15f903bf6b954756081f19f
Uncensored-Developer <osezele.o@gmail.com>
2020-06-04 14:03:32 +0000

    Add VerificationCode model

---
+++ src/models/verificationCodes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+module.exports = (sequelize, DataTypes) => {
+    const VerificationCode = sequelize.define('VerificationCode', {
+        code: {
+            type: DataTypes.STRING(6),
+            allowNull: false,
+        },
+
+    }, {});
+    // eslint-disable-next-line no-unused-vars
+    VerificationCode.associate = function (models) {
+        // associations can be defined here
+    };
+    return VerificationCode;
+};
